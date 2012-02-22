b661a790dbb252efcaeb910cde4cf9368102a2b9
Martin Peschke <martin@friendfund.com>
2012-02-22 18:14:15 +0100

    adding proc test and RemoteProc fixes

--- jsonmapper/backend.py
+++ jsonmapper/backend.py
@@ -4,20 +4,30 @@ import logging
 log = logging.getLogger(__name__)
 
 class DBMessage(Exception):
-    def __init__(self, message, returnobj):
-        self.returnobj = returnobj
+    def __init__(self, message, result_key):
+        self.result_key = result_key
         self.message = message
     def __str__(self):
       return "<DBMessage: '{0}'>".format(self.message)
 class DBException(Exception):pass
 
+class RemoteProc(object):
+  def __init__(self, remote_path, method, root_key, result_cls):
+    self.remote_path = remote_path
+    self.method      = method
+    self.root_key    = root_key
+    self.result_cls  = result_cls  
+  def __call__(self, backend, data):
+    return self.result_cls.wrap(backend(self.root_key, url=self.remote_path, method=self.method, data=data))
+
+
 class Backend(object):
   def __init__(self, location):
     self.location = location
   
-  def __call__(self, returnobj, **options):
+  def __call__(self, result_key, **options):
     result = self.query(**options)
-    return result[returnobj]
+    return result[result_key]
 
   def query(self, **options):
     h = Http()
