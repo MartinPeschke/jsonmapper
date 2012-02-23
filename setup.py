7cddd24eabfc9674d4d2306fa50db8ea5d99307d
Martin Peschke <martin@friendfund.com>
2012-02-23 19:36:16 +0100

    removing zip_safe

--- setup.py
+++ setup.py
@@ -14,13 +14,14 @@ from setuptools import setup
 
 setup(
     name = 'JSONMapperHGMMPFork',
-    version = '0.17',
+    version = '0.18',
     description = "Mapping JSON to objects and vice versa, this is a product of Christopher Lenz and Matthew Good with slight modifications",
     long_description = '',
     author = 'Martin Peschke',
     author_email = 'martin@per-4.com',
     requires=['simplejson', 'httplib2', 'formencode', 'pyramid', 'mako'],
     license = 'BSD',
+    zip_safe = False,
     url = 'https://github.com/MartinPeschke/jsonmapper',
     packages = ['jsonmapper'],
     data_files=[('jsonmapper/templates', ['jsonmapper/templates/forms.html'])]
