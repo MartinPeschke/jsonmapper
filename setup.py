bca23411b8c4e67e2209f09cc4d1f26ef305ba6a
Martin Peschke <martin@friendfund.com>
2012-02-23 19:17:13 +0100

    adding basehandler and form libraries

--- setup.py
+++ setup.py
@@ -14,12 +14,12 @@ from setuptools import setup
 
 setup(
     name = 'JSONMapperHGMMPFork',
-    version = '0.12',
+    version = '0.15',
     description = "Mapping JSON to objects and vice versa, this is a product of Christopher Lenz and Matthew Good with slight modifications",
     long_description = '',
     author = 'Martin Peschke',
     author_email = 'martin@per-4.com',
-    requires=['simplejson', 'httplib2'],
+    requires=['simplejson', 'httplib2', 'formencode', 'pyramid', 'mako'],
     license = 'BSD',
     url = 'https://github.com/MartinPeschke/jsonmapper',
     packages = ['jsonmapper'],
