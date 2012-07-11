2df9b8f5d5b26deab8f681d5d612b200276b0fde
Martin Peschke <martin@per-4.com>
2012-07-11 14:56:09 +0200

    empty list fix

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import setup
 
 setup(
     name = 'JSONMapperHGMMPFork',
-    version = '0.50',
+    version = '0.51',
     description = "Mapping JSON to objects and vice versa, this is a product of Christopher Lenz and Matthew Good with slight modifications",
     long_description = '',
     author = 'Martin Peschke',
